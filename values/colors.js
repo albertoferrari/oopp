9a247c4697851e770b1b8e6da67747dd6d17a6ee
Alberto Ferrari <alberto.ferrari@unipr.it>
2018-05-16 17:28:53 +0200

    Add files via upload

---
+++ values/colors.js
@@ -0,0 +1,41 @@
+/**
+ * CLASS
+ *   FIELD
+ *     TYPE
+ *     OBJ
+ *   CONSTR
+ *   	PARAM
+ *   METH
+ */
+
+var cc_ = {
+	class: 0,
+	interface: 280,
+	package: 180,
+	method: 200,
+	constr: 180,
+}
+var OOPP_COL = {
+
+	classes_set: cc_.class,
+	classes_get: cc_.class,
+	classes_mutatorcontainer: 310,
+	classes_mutatorinterface: cc_.interface,
+	classes_mutatorpackage: cc_.package,
+	classes_mutatorclass: cc_.class,
+	object_method_return: cc_.method,
+	object_method: cc_.method,
+	classes_constructor: cc_.constr,
+	classes_parameter: 220,
+	classes_field: 20,
+	object_type: 45,
+
+	interfaces_get: cc_.interface,
+	interfaces_set: cc_.interface,
+
+	objects_mutatorcontainer2: 310,
+	objects_mutatorinterface2: cc_.interface,
+	objects_mutatorpackage2: cc_.package,
+	object_constant: 110,
+	object_string: cc_.class,
+}
---
+++ values/globals.js
@@ -0,0 +1,23 @@
+var OOPP_VERSION = '2.0';
+var OOPP_ENABLE_AUTOMATIC_CODE_GEN = true;
+var OOPP_BLOCKLY_SETTINGS = {
+	rtl: false,
+	media: './media/',
+	toolbox: null,		// Defined later
+	zoom: {
+		controls: true,
+		wheel: true,
+	scaleSpeed: 0.96,
+	reverse: true
+  	},
+	comments: true,
+	disable: true,          // Allow blocks to be disabled
+	collapse: true,         // Allow blocks to be collapsed
+	sounds: false,        // Come on..
+	grid: {
+		spacing: 25,
+		length: 3,
+		colour: '#ccc',
+		snap: true
+	},
+};
